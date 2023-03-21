a9892ff548614ca3d2ce38efcde46fa1894ac2f0
tckiprotich <tckiprotich@gmail.com>
2023-03-21 16:50:20 +0300

    oject relatioal mappig

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
-
-<<<<<<< HEAD
-"""
-    A script that lists all states from the database hbtn_0e_0_usa
-    Username, password and database names are given as user args
-"""
-
-
-import sys
 import MySQLdb
+import sys
 
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    data = cursor.fetchall()
-
-    for row in data:
+    for row in cursor.fetchall():
         print(row)
 
     cursor.close()
     db.close()
-=======
-
-import MySQLdb
-from sys import argv
-
-'''
-a script that lists all states
-from the database
-'''
-if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
-    cursor.close()
-    db.close()
-
->>>>>>> b95bb6843f82300f1bcc5e44f6aff07f0f6e7031
