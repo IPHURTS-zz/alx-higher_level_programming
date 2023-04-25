07dd3a385695d7f08e1defb9b2167e0a5347eccb
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-25 04:02:48 +0000

    json format

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,32 +1,15 @@
 #!/usr/bin/node
-
-/*
-script that prints the number of movies where the character “Wedge Antilles” is present.
-
-The first argument is the API URL of the Star wars API: https://swapi-api.alx-tools.com/api/films/
-Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
-You must use the module request
- */
+// Prints the title of a Star Wars movie where the episode number
+// matches the given integer
 
 const request = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/;";
-const wedge = "https://swapi-api.alx-tools.com/api/people/18/";
+const argv = process.argv[2];
+let url = "https://swapi-api.alx-tools.com/api/films/";
 
-request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        let count = 0;
-        const films = JSON.parse(body).results;
-        for (const film of films) {
-            for (const character of film.characters) {
-                if (character === wedge) {
-                    count++;
-                }
-            }
-        }
-        console.log(count);
-    }
-}
-);
-
+request(url + argv, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  let json = JSON.parse(body);
+  console.log(json.title);
+});
